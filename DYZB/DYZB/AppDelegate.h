aa0af669beaee33fc8e0983b7d28cb2078942378
CCYSmile <cuichangyun@CCYdeMacBook-Air.local>
2016-11-03 14:04:07 +0800

    first push

---
+++ DYZB/DYZB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DYZB
+//
+//  Created by CCY on 2016/11/3.
+//  Copyright © 2016年 SKYDispark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DYZB/DYZB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DYZB
+//
+//  Created by CCY on 2016/11/3.
+//  Copyright © 2016年 SKYDispark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
